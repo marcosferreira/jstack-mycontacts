f78ffa83f9af9271fce563b04eca652b9a49187b
Marcos Ferreira <marcosferreira.developer@gmail.com>
2021-12-26 08:24:27 -0300

    Configuring Eslint and editorconfig

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ const app = express();
 
 app.get('/', (request, response) => response.send('Hello Wolrd'));
 
-app.listen(3333, () => console.log('🔥🔥 Server started at http://localhost:3333'));
+app.listen(3333);
